9aa30a593d00ccde66350976279b2968930aeb63
Eenraii <henryvarley14@gmail.com>
2023-10-28 00:48:41 +0100

    planning the logic of moving balls

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    /* PLAN:
+     * onClick(Tube) = select tube & find out what top ball is 
+     * onClick(secondTube) tube.full ? if yes cannot move ball if no is ball colour matching? yes = move ball no = don't move ball
+     */
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
