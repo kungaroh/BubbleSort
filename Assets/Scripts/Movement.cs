4e5950d2e40b953e88bf4a4f7ee8819cdde13d31
Henry <HenryVarley14@gmail.com>
2023-10-30 15:26:04 +0000

    more function planning really

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -1,6 +1,8 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class Movement : MonoBehaviour
 {
@@ -8,16 +10,53 @@ public class Movement : MonoBehaviour
      * onClick(Tube) = select tube & find out what top ball is 
      * onClick(secondTube) tube.full ? if yes cannot move ball if no is ball colour matching? yes = move ball no = don't move ball
      */
+    Camera thisCamera;
+    public GameObject Tubes;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        thisCamera = GetComponent<Camera>();
+        Debug.Log(Tubes.transform.childCount);
+        foreach (Transform child in Tubes.transform)
+        {
+            ballTracker(child.gameObject);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+        if (Input.GetMouseButton(0))
+        {
+            PointerEventData pointer = new PointerEventData(EventSystem.current);
+            pointer.position = Input.mousePosition;
+
+            List<RaycastResult> raycastResults = new List<RaycastResult>();
+            EventSystem.current.RaycastAll(pointer, raycastResults);
+
+            if (raycastResults.Count > 0)
+            {
+                foreach (var go in raycastResults)
+                {
+                    if (go.gameObject.name.StartsWith("Tube"))
+                    {
+                        ballSelector();//get balls and select the top one then increase height of the top ball to the top of the tube
+                    }
+                }
+            }
+        }
+    }
+
+    void ballTracker(GameObject Tube) // when called this function should track where all the balls are
+    {
+        //detect all collisions, identify the balls and add them to an array, then sort them based on height
+    }
+
+    void ballSelector()
+    {
+        //select ball
     }
 }
+
