8aa4c54bc7f31f82ff5e3525b1bc39ddaed47422
Henry <HenryVarley14@gmail.com>
2024-09-13 18:04:51 +0100

    Refactor of ball movement system

--- Assets/Scripts/MovementScript.cs
+++ Assets/Scripts/MovementScript.cs
@@ -1,41 +1,42 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.EventSystems;
-using UnityEngine.SceneManagement; 
+using UnityEngine.XR;
 
 public class MovementScript : MonoBehaviour
 {
-    
-    Camera thisCamera;
-    public GameObject Tubes;
-    private bool isBallSelected = false;
-    public GameObject selBall;
-    public Rigidbody2D ballFreeze;
-    GameObject currGO;
-    GameObject currBall;
-    int numFin;//number of tubes that have been finished (all balls the same colour inside
-    static Scene currentLevel;
-    public GameObject finishedMenuUI;
-    
+    private Camera _thisCamera;
+    [SerializeField] private GameObject tubes;
+    private bool _isBallSelected = false;
+    private GameObject _selectedBall;
+    private Rigidbody2D _rbBall;
+    private GameObject _currentTube;
+    private GameObject _previousBall;
+    private int _countCompleteTubes;
+    private static Scene _currentLevel;
+    [SerializeField] private GameObject levelCompleteMenuUI;
 
-    Dictionary<string, List<GameObject>> ballTube = new Dictionary<string, List<GameObject>> { };
+    // The dictionary where the key is the name of the tube and the data is the balls in a stack
+    private readonly Dictionary<string, Stack<GameObject>> _dictionaryBallTube = new Dictionary < string, Stack<GameObject>> { };
+
 
     // Start is called before the first frame update
     void Start()
     {
-        thisCamera = GetComponent<Camera>();
-        
-        foreach (Transform child in Tubes.transform)
+        _thisCamera = GetComponent<Camera>();
+
+        foreach (Transform child in tubes.transform)
         {
             ballTracker(child.gameObject);
         }
-         currentLevel = SceneManager.GetActiveScene();
-        numFin = 0;
 
-        
+        _currentLevel = SceneManager.GetActiveScene();
+        _countCompleteTubes = 0;
+
     }
 
     // Update is called once per frame
@@ -57,224 +58,180 @@ public class MovementScript : MonoBehaviour
                     {
                         if (go.gameObject.name.StartsWith("Tube"))
                         {
-                            ballSelector(go.gameObject);//get balls and select the top one then increase height of the top ball to the top of the tube
-                            currGO = go.gameObject;
+                            ballSelector(go.gameObject); // get balls and select the top one then increase height of the top ball to the top of the tube
+                            _currentTube = go.gameObject;
                         }
                     }
                 }
             }
         }
     }
-
-
     
-    void ballTracker(GameObject Tube) // when called this function looks at all child objects of a Tube and adds them to the dictionary - it's only really used to create the dictionary to start the level
+    // Adds the child objects of tubes and adds them to the dictionary
+    void ballTracker(GameObject tube)
     {
-        string keyName = Tube.name;
-        List<GameObject> dicList;
+        string keyName = tube.name;
+        // Creates the stack that will be added to the dictionary
+        Stack<GameObject> dictStack = new Stack<GameObject>();
+        List<GameObject> tempList = new List<GameObject>();
+
+        foreach (Transform child in tube.transform)
+        {
+            tempList.Add(child.gameObject);
+        }
+        //Orders the list by "height" 
+        tempList = tempList.OrderBy(ball => ball.transform.localPosition.y).ToList();
+
         
-        foreach (Transform child in Tube.transform)
+        for(int i =0; i < tempList.Count; i++)
         {
-            if (!ballTube.TryGetValue(keyName, out dicList))
-            {
-                dicList = new List<GameObject>();
-                dicList.Add(child.gameObject);
-                ballTube.Add(keyName, dicList);
-
-                ballTube[keyName] = ballTube[keyName].OrderBy(y => Vector2.Distance(Tube.transform.localPosition, y.transform.localPosition)).ToList();
-            }
-            else
-            {
-                ballTube[keyName].Add(child.gameObject);
-
-                ballTube[keyName] = ballTube[keyName].OrderBy( y => Vector2.Distance(Tube.transform.localPosition, y.transform.localPosition)).ToList();
-            }
-        }//then sort them based on height
-
+            dictStack.Push(tempList[i]);
+        }
+        _dictionaryBallTube.Add(keyName, dictStack);
     }
 
-
-
-    void ballSelector(GameObject Tube)
+    void ballSelector(GameObject tube)
     {
-        string keyName = Tube.name;
-        List<GameObject> dicList;
-        Vector2 selBallPos;
+        string keyName = tube.name;
+        Stack<GameObject> dictStack = _dictionaryBallTube[keyName];
 
-        if (ballTube.ContainsKey(keyName) == true)
+        // if tube is empty move the ball
+        if (dictStack.Count == 0)
         {
-            dicList = ballTube[keyName];
-
-            if (ballTube[keyName].Count == 0) //this runs when an existing tube becomes empty and you're trying to put a ball in the empty tube
-            {
-                Debug.Log("Tube is empty: ");
-
-                ballTube[keyName].Add(selBall);
-
-                ballFreeze = selBall.GetComponent<Rigidbody2D>();
-
-                selBall.transform.position = new Vector2(Tube.transform.position.x, Tube.transform.position.y + 2);
-                ballFreeze.simulated = true;
-
-                ballTube[currGO.name].Remove(selBall);
-
-                Debug.Log(selBall + " has been removed from " + currGO + " and added to " + keyName);
-                currBall = selBall;
-                selBall = null;
-                isBallSelected = false;
-                Debug.Log("ball selected: " + isBallSelected + "Ball simulated: " + selBall.GetComponent<Rigidbody2D>().simulated);
-                //issue here where when ball is moved into the empty tube, in unity it doesn't become simulated even though here it does
-
-            }
-            else //this runs if a tube is not empty
-            {
-                selBall = dicList[0];
-            }
-
-
-            if (isBallSelected == false & currBall == null) // this runs if no ball is selected and there have been no balls previously selected (first move)
-            {
-                Debug.Log("Valid tube. No ball selected. currBall = null");
-                selBallPos = selBall.transform.position;
-                selBall.transform.position = new Vector2(selBallPos.x, Tube.transform.position.y + 2);
-                ballFreeze = selBall.GetComponent<Rigidbody2D>();
-                ballFreeze.simulated = false;
-
-                isBallSelected = true;
-                currBall = selBall;
-            }
-            else if (isBallSelected == false & currBall != selBall & currBall != null) // this runs when a no ball is selected, but a ball has previously been used (e.g. just moved a ball and are selecting a new ball
-            {
-                Debug.Log("Valid tube. No ball selected. ");
-                selBallPos = selBall.transform.position;
-                selBall.transform.position = new Vector2(selBallPos.x, Tube.transform.position.y + 2);
-                ballFreeze = selBall.GetComponent<Rigidbody2D>();
-                ballFreeze.simulated = false;
-
-                isBallSelected = true;
-                currBall = selBall;
-
-            }
-            else if (isBallSelected == true & currBall != selBall & currBall != null)// this runs when a ball is selected and you click another tube
-            {
-                if (currBall.tag == selBall.tag & ballTube[keyName].Count < 4) // this checks if the balls have the same tag(colour) and the tube is not full, in order to move the ball
-                {
-                    Debug.Log("Valid tube. ball selected. Balls has same tag ");
-                    ballTube[keyName].Add(currBall);
-                    ballTube[currGO.name].Remove(currBall);
-
-                    ballTube[keyName] = ballTube[keyName].OrderBy(y => Vector2.Distance(Tube.transform.localPosition, y.transform.localPosition)).ToList();
-
-                    Debug.Log("Tube added to list");
-
-                    currBall.transform.position = new Vector2(Tube.transform.position.x, Tube.transform.position.y + 2);
-                    ballFreeze.simulated = true;
-                    Debug.Log(currBall + " has been removed from " + currGO + " and added to " + keyName);
-                    isBallSelected = false;
-                    currBall = selBall;
-                    Debug.Log(ballTube[keyName].Count);
-                    if (ballTube[keyName].Count == 4)
-                    {
-                        finChecker(Tube);
-                    }
-                }
-                else //this runs if the balls are different colours or the tube is full (drops selected ball and re-runs to select new ball)
-                {
-                    Debug.Log("Valid Tube. Ball Selected. Different tag or tube is already full");
-                    ballFreeze.simulated = true;
-                    isBallSelected = false;
-
-                    ballSelector(Tube);
-                }
-            }
-            else if ( isBallSelected == false & currBall == selBall & ballFreeze.simulated == true) //this causes ball dropping issue
-            {
-                Debug.Log("When does this run?");
-                selBallPos = selBall.transform.position;
-                selBall.transform.position = new Vector2(selBallPos.x, Tube.transform.position.y + 2);
-                ballFreeze = selBall.GetComponent<Rigidbody2D>();
-                ballFreeze.simulated = false;
-
-                isBallSelected = true;
-                currBall = selBall;
-            }
-            else if (isBallSelected == true & currBall == selBall) // this runs if you select the same ball, and drops the ball, unselecting it
-            {
-                Debug.Log("Valid tube. Ball Selected. Same ball");
-
-
-                ballFreeze.simulated = true;
-                isBallSelected = false;
-                currBall = selBall;
-            }
+            if (_isBallSelected)
+                HandleBallMove(tube, keyName);
+            
+            return;
         }
+        //select top ball from tube
+        _selectedBall = dictStack.Peek();
 
-        else if (isBallSelected == true) //this sections runs if a ball has been selected and the tube is not in the dictionary (moving a ball to an empty tube at the start of the level
+        // First ball picked up
+        if (!_isBallSelected & _previousBall == null)
         {
-            Debug.LogWarning("Tube does not exist");
-            Debug.Log(selBall);
-            dicList = new List<GameObject>();
-            dicList.Add(selBall);
-            ballTube.Add(keyName, dicList);
-
-            ballTube[keyName] = ballTube[keyName].OrderBy(y => Vector2.Distance(Tube.transform.localPosition, y.transform.localPosition)).ToList();
-
-            Debug.Log("Tube added to list");
-
-            selBall.transform.position = new Vector2(Tube.transform.position.x, Tube.transform.position.y + 2);
-            ballFreeze.simulated = true;
-
-            ballTube[currGO.name].Remove(selBall);
-            Debug.Log(selBall + " has been removed from " + currGO + " and added to " + keyName);
-
-            isBallSelected = false;
+            SelectBall(tube);
         }
+        // Picking up new ball
+        else if (!_isBallSelected & _previousBall != _selectedBall)
+        {
+            SelectBall(tube);
+        }
+        // Holding a new ball, selected different tube
+        else if (_isBallSelected & _previousBall != _selectedBall)
+        {
+            HandleBallMove(tube, keyName);
+        }
+        // Selected a ball, selected same tube
+        else if (_isBallSelected & _previousBall == _selectedBall)
+        {
+            DeselectBall(tube);
+        }
+        // shouldn't ever happen
+        /*else if (!_isBallSelected & _previousBall == _selectedBall & _rbBall.simulated)
+        {
+            Debug.Log("Something has gone horribly wrong");
+            SelectBall(tube);
+        }*/
     }
 
-    public void finChecker(GameObject Tube)
+    void SelectBall(GameObject tube)
     {
-        string keyName = Tube.name;
+        Debug.Log($"Picking up {_selectedBall}");
+        Vector2 selectedBallPos = _selectedBall.transform.position;
+        _selectedBall.transform.position = new Vector2(selectedBallPos.x, tube.transform.position.y + 2);
+        _rbBall = _selectedBall.GetComponent<Rigidbody2D>();
+        _rbBall.simulated = false;
+        _isBallSelected = true;
+        _previousBall = _selectedBall;
+    }
+
+    void HandleBallMove(GameObject tube, string keyName)
+    {
+        if (_previousBall.CompareTag(_selectedBall.tag) & _dictionaryBallTube[keyName].Count < 4)
+        {
+            Debug.Log($"Moving {_previousBall} to {keyName}");
+            _dictionaryBallTube[keyName].Push(_previousBall);
+            _dictionaryBallTube[_currentTube.name].Pop();
+            MoveBall(tube);
+            _isBallSelected = false;
+            _previousBall = _selectedBall;
+            _selectedBall = null;
+            if (_dictionaryBallTube[keyName].Count == 4)
+            {
+                FinChecker(tube, keyName);
+            }
+        }
+        else
+        {
+            Debug.Log($"{_previousBall} not moved (different tag or full tube)");
+            Debug.Log($" TAG CHECK: {_previousBall.CompareTag(_selectedBall.tag)} COUNT CHECK: {_dictionaryBallTube[_currentTube.name].Count}");
+            _rbBall.simulated = true;
+            _isBallSelected = false;
+            ballSelector(tube);
+            
+        }
+    }
+    
+    void MoveBall(GameObject tube)
+    {
+        _previousBall.transform.position = new Vector2(tube.transform.position.x, tube.transform.position.y + 2);
+        _rbBall.simulated = true;
+    }
+
+    void DeselectBall(GameObject tube)
+    {
+        Debug.Log($"Deselecting {_selectedBall.name}");
+        _rbBall.simulated = true;
+        _isBallSelected = false;
+        _previousBall = _selectedBall;
+    }
+
+    void FinChecker(GameObject tube, string keyName)
+    {
         bool tagsMatch = false;
         GameObject lastBall = null;
-        if (numFin != ballTube.Count - 2)
+        
+        foreach (GameObject ball in _dictionaryBallTube[keyName])
         {
-            foreach (GameObject ball in ballTube[keyName])
+            if (lastBall == null)
             {
-                if (lastBall == null)
-                {
-                    lastBall = ball;
-                }
-                else if (lastBall != null & ball.tag == lastBall.tag)
-                {
-                    tagsMatch = true;
-                    lastBall = ball;
-                    Debug.Log("tags match");
-                }
-                else if (lastBall != null & ball.tag != lastBall.tag)
-                {
-                    tagsMatch = false;
-                    lastBall = ball;
-                    Debug.Log("tags don't match");
-                    break;
-                }
+                lastBall = ball;
             }
-            if (tagsMatch == true)
+            else if (ball.CompareTag(lastBall.tag))
             {
-                Debug.Log("tags match and numfin increase");
-                numFin++;
+                tagsMatch = true;
+                lastBall = ball;
+                Debug.Log("tags match");
+            }
+            else if (lastBall != null & !ball.CompareTag(lastBall.tag))
+            {
+                tagsMatch = false;
+                lastBall = ball;
+                Debug.Log("tags don't match");
+                break;
             }
         }
-        if(numFin == ballTube.Count -2)
+
+        if (!tagsMatch)
+            return;
+        
+        
+        Debug.Log("tags match and completed tubes increases");
+        _countCompleteTubes++;
+        
+            
+        if(_countCompleteTubes == _dictionaryBallTube.Count -2)
         {
             Debug.Log("Level Complete");
-            finishedMenuUI.SetActive(true);
+            levelCompleteMenuUI.SetActive(true);
             ButtonScript.gameIsPaused = true;
         }
     }
+    
     public static IEnumerator LoadNextLevel()
     {
         Debug.Log("LoadNextLevel Called");
-        int levelNum = currentLevel.buildIndex;
+        int levelNum = _currentLevel.buildIndex;
         int buildIndex = SceneUtility.GetBuildIndexByScenePath($"Level {levelNum}"); //this works because the buildIndex is always 1 above the current level name e.g. level 1 is build index 2
         
         if (levelNum > 10)
